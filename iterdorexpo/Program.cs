81163e81b1404ba2c2ef213bb5345118df8aab9f
erick mera <eriieericc@gmail.com>
2020-08-07 12:11:22 -0500

    primer compromiso

---
+++ iterdorexpo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace iterdorexpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CContenedora  datos = new CContenedora();
+            foreach (int valor in datos)
+            {
+                Console.WriteLine(valor);
+
+            }
+        }
+    }
+}
---
+++ iterdorexpo/contenedora.cs
@@ -0,0 +1,66 @@
+﻿
+using iterdorexpo;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iterdorexpo
+{
+    class CContenedora : IEnumerable
+    {
+        private int[] valores = new int[10];
+
+        public CContenedora()
+        {
+            for (int n = 0; n < 10; n++)
+                valores[n] = n * n;
+        }
+
+        // Implementamos el GetEnumerator necesario por IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // Instanciamos el enumerador y lo regresamos
+       
+            return (new Contenedorenum(valores));
+        }
+    }
+
+    class Contenedorenum: IEnumerator  
+    {
+        public int[] arreglo;
+        private int posicion = -1;
+
+        public Contenedorenum (int[] pArreglo)
+        {
+            arreglo = pArreglo;
+        }
+
+        // Indica con true o false si hay siguiente
+        public bool MoveNext()
+        {
+            posicion++;
+            if (posicion < arreglo.Length)
+                return true;
+            else
+                return false;
+        }
+
+        // Nos devuelve al inicio de la coleccion
+        public void Reset()
+        {
+            posicion = -1;
+        }
+
+        // Devuelve el elemento en el que estamos parados
+        public object Current
+        {
+            get
+            {
+                return arreglo[posicion];
+            }
+        }
+    }
+}
+
+
